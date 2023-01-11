ff58828cd188c46765c90ababaab4268f13bdeff
artem_lifshits <artem.lifshits@t-systems.com>
2023-01-11 23:55:10 +0300

    message changes

--- pkg/conv/payload_conversion.go
+++ pkg/conv/payload_conversion.go
@@ -7,25 +7,24 @@ import (
 
 func ConvertPushPayload(payload interface{}) (msg string) {
 	body := payload.(github.PushPayload)
-	msg = fmt.Sprintf("New push by %s, created at: %d, commits: %v", body.Pusher.Name, body.Repository.CreatedAt, body.Commits)
+	msg = fmt.Sprintf("New push by '%s', created at: %s, commit: %v", body.Pusher.Name, body.HeadCommit.Timestamp, body.HeadCommit.URL)
 	return
 }
 
 func ConvertPullRequestPayload(payload interface{}) (msg string) {
 	body := payload.(github.PullRequestPayload)
-	msg = fmt.Sprintf("%v", body)
+	msg = fmt.Sprintf("New pull request created at: %s, url: %s", body.PullRequest.CreatedAt, body.PullRequest.URL)
 	return
 }
 
 func ConvertPingPayload(payload interface{}) (msg string) {
 	body := payload.(github.PingPayload)
-	msg = fmt.Sprintf("%v", body)
-
+	msg = fmt.Sprintf("New ping sent at %s", body.Hook.CreatedAt)
 	return
 }
 
 func ConvertCommitCommentPayload(payload interface{}) (msg string) {
 	body := payload.(github.CommitCommentPayload)
-	msg = fmt.Sprintf("%v", body)
+	msg = fmt.Sprintf("New commit comment by %s created at %s\n Message: %s", body.Comment.User.Login, body.Comment.CreatedAt, body.Comment.Body)
 	return
 }
